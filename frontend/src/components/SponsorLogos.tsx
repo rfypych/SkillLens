39cc51c7697943dc0dfecedb4440f69491515b79
unknown <125186835+rfypych@users.noreply.github.com>
2026-07-12 09:59:39 +0700

    style: make JHIC logo most prominent in sponsor lineup

--- frontend/src/components/SponsorLogos.tsx
+++ frontend/src/components/SponsorLogos.tsx
@@ -3,7 +3,7 @@
 export default function SponsorLogos() {
   return (
     <div className="flex flex-wrap items-center justify-center gap-6 py-6 border-t border-brand-gray-light/30 mt-auto">
-      <img src="/sponsors/jhic.png" alt="JHIC" className="h-6 md:h-8 opacity-50 grayscale hover:grayscale-0 hover:opacity-100 transition-all duration-300 object-contain" />
+      <img src="/sponsors/jhic.png" alt="JHIC" className="h-10 md:h-14 opacity-50 grayscale hover:grayscale-0 hover:opacity-100 transition-all duration-300 object-contain" />
       <img src="/sponsors/jagoanhosting.png" alt="Jagoan Hosting" className="h-6 md:h-8 opacity-50 grayscale hover:grayscale-0 hover:opacity-100 transition-all duration-300 object-contain" />
       <img src="/sponsors/komdigi.png" alt="Komdigi" className="h-6 md:h-8 opacity-50 grayscale hover:grayscale-0 hover:opacity-100 transition-all duration-300 object-contain" />
       <img src="/sponsors/garudaspark.png" alt="Garuda Spark" className="h-6 md:h-8 opacity-50 grayscale hover:grayscale-0 hover:opacity-100 transition-all duration-300 object-contain" />
